5281948f334b3bfec487ad1ca0ebbcd8c2685549
Naphorn Chokpokasombat <31033972+Naphorn@users.noreply.github.com>
2017-10-11 19:20:49 +0700

    CPP06 Electrical .a

---
+++ Electrical/Power.h
@@ -0,0 +1,11 @@
+// Power.h
+
+namespace Power
+{
+   class MyPower
+    {
+    public:
+        // Returns e * i
+        static double Multiply(double e, double i);
+    };
+}
---
+++ Electrical/power.cpp
@@ -0,0 +1,13 @@
+#include "Power.h"
+
+#include <stdexcept>
+
+using namespace std;
+
+namespace Power 
+{
+    double MyPower::Multiply(double e, double i)
+    {
+        return e * i;
+    }
+}
